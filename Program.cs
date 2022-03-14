3e6d88b0d403a9152dfeb145abb7276c04b40a8b
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:07:37 +0530

    ADDED:Year is Leap Or Not

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program
+{
+    public class LeapYear
+    {
+        int year;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Year : ");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("is a LeapYear", year);
+            }
+            else
+            {
+                Console.WriteLine("not Leapyear", year);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
