5c436d0971a6b8579f61dd718ed9f23721dbb717
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:18:31 +0530

    ADDED:Calculate Nth Harmonic value

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Program
+{
+    public class HarmonicNumber 
+    {
+        public static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Enter the Harmonic Value : ");
+            int harmonicNo = int.Parse(Console.ReadLine());
+            double total = 0;
+
+            //Repeat until the Harmonic Sequence complete
+            for (int i = 1; i <= harmonicNo; i++)
+            {
+                total += (double)1 / i;
+                Console.WriteLine("1/" + i + "+");
+            }
+            Console.WriteLine("\nTotal of Harmonic Number :" + total);
+
+        }
+    }
+}
