08c9a1a5df7414b346d95eb8f15df47f4e042fd4
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:28:29 +0530

    ADDED:Number Is Even Or Odd

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    public class EvenOrOdd
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Enter Any Number :");
+            number = int.Parse(Console.ReadLine());
+
+            //Checking for Even or Odd Number using Mod Operator
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The Number " + number + " is Even.");
+            }
+            else
+            {
+                Console.WriteLine("The Number " + number + " is Odd.");
+            }
+
+        }
+    }
+}
