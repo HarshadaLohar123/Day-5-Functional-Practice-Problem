7b6486ffc3f42f5fab54465d67f2965298c2613b
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:23:47 +0530

    ADDED:Quotient Or Remainder

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    public class QuotientOrRemainder
+    {
+        public static void Main(string[] args)
+        {
+            //Variable Declaration
+            int dividend, divisor, quotient, remainder;
+
+            Console.WriteLine("Enter the Value of Dividend :");
+            dividend = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Value of Divisor :");
+            divisor = int.Parse(Console.ReadLine());
+
+            //Formula to get Quotient Or Remainder
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+
+            //Displaying the value of Quotient Or Remainder
+            Console.WriteLine("Quotient =" + quotient);
+            Console.WriteLine(("Remainder =" + remainder));
+
+        }
+    }
+}
