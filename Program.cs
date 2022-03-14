cae0ce9e9304f0154801053785d33ccecdd74ab9
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:32:17 +0530

    ADDED:Alphabet Is Vowel Or Consonant

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlphabetProgram
+{
+    public class VowelOrConsonant
+    {
+        public static void Main(string[] args)
+        {
+            char ch;
+        
+            Console.WriteLine("Enter Any Character :");
+            string input = Console.ReadLine();
+            ch = Convert.ToChar(input.ToLower());
+
+            //Checking for Character is Vowel or Consonant
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine(ch + " is Vowel");
+            }
+            else
+            {
+                Console.WriteLine(ch + " is Consonant");
+            }
+
+        }
+    }
+}
