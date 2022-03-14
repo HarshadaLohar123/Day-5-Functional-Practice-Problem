ff949b7e1780a66e4b380c88380a1da77ec79d4e
harshadalohar123 <harshalohar123@gmail.com>
2022-03-14 15:26:02 +0530

    ADDED:Swapping Of Two Number

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program
+{
+    public class SwapTwoNumber
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, c;
+
+
+            Console.WriteLine("Enter the Value for A :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value for B :");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Value of A and B Before Swapping");
+            Console.WriteLine("A : " + a + "\t" + "B : " + b);
+
+            //Performing Sqpping logic using 3 Variables
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("Values of A and B After Swapping");
+            Console.WriteLine("A : " + a + "\t" + "B : " + b);
+
+        }
+    }
+}
